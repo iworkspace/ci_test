c166b7c0843879293408e320a02334d6ac6303fb
jia.he <iworkspace@163.com>
2021-07-29 11:41:51 +0800

    dns?

--- test.sh
+++ test.sh
@@ -1,7 +1,19 @@
 #/bin/bash
 set +e
+
+whoami
+pwd
+
 ip a
 ip r
-ping -c 3 www.google.com 
+wget www.google.com 
 nslookup workspace.noip.cn
-exit 0
+ping -c 3 workspace.noip.cn
+
+echo "nameserver 8.8.8.8" > /etc/resolve.conf
+ping  -c 3 www.google.com
+ping -c 3 75.126.150.210
+ping -c 3 2001::a27d:5207
+
+
+netstat -ltnup
